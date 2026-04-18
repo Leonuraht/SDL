31d559428b2529eb0512564bfebbca65f75f7d9a
Leonuraht <blinderoggy@google.com>
2026-04-19 00:12:16 +0530

    Create Repo

---
+++ build/CMakeFiles/SDL_GPU_App.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDL_GPU_App.dir/link.d"
+  "CMakeFiles/SDL_GPU_App.dir/src/main.cpp.o"
+  "CMakeFiles/SDL_GPU_App.dir/src/main.cpp.o.d"
+  "SDL_GPU_App"
+  "SDL_GPU_App.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDL_GPU_App.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
