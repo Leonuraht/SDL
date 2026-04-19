dd08bb0c753905489b0a66ffd7ca2b50f7dfc205
Leonuraht <blinderoggy@google.com>
2026-04-19 17:02:35 +0530

    chang1

---
+++ include/init.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+void initer();
+
+void exiter();
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_video.h>
+#include <iostream>
 
-int main() { return 0; }
+int width = 800, height = 600;
+
+int main() {
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window *window =
+      SDL_CreateWindow("Ray Tracer", width, height, SDL_WINDOW_RESIZABLE);
+  if (window == NULL)
+    std::cout << "\nWINDOW CREATION FAILED\n" << std::endl;
+  SDL_Surface *surf = SDL_LoadSurface("~/Downloads/immg.png");
+  return 0;
+}
